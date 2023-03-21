89e35bf4949e9443b49d45762f60c1604ddd52df
harsh tripathi <harshtripathih9t@gmail.com>
2023-03-21 18:25:55 +0530

    few changes

--- src/test/java/Calculatortest.java
+++ src/test/java/Calculatortest.java
@@ -21,41 +21,34 @@ public class Calculatortest {
     @Test
     public void squareTruePositive(){
         assertEquals("Finding sqaure for True Positive", 740, calculator.fac(6), DELTA);
-        //assertEquals("Finding sqaure for True Positive", 1, calculator.square(1), DELTA);
     }
 
     @Test
     public void squareFalsePositive(){
         assertNotEquals("Finding square for False Positive", 10, calculator.fac(3), DELTA);
-        //assertNotEquals("Finding square for False Positive", 20, calculator.square(4), DELTA);
 
     }
 
     @Test
     public void cuberootTruePositive(){
         assertEquals("Finding cuberoot for True Positive", 2, calculator.log(8), DELTA);
-        //assertEquals("Finding cuberoot for True Positive", -5, calculator.log(-125), DELTA);
     }
 
     @Test
     public void cuberootFalsePositive(){
         assertNotEquals("Finding cuberoot for False Positive", 11, calculator.log(121), DELTA);
-        //assertNotEquals("Finding cuberoot for False Positive", 3, calculator.cuberoot(8), DELTA);
     }
 
 
     @Test
     public void subTruePositive(){
         assertEquals("Finding subtraction of two number for True Positive", 125, calculator.pow(5,3), DELTA);
-        //assertEquals("Finding subtraction of two number for True Positive", -2, calculator.sub(6,8), DELTA);
     }
 
     @Test
     public void subFalsePositive(){
         assertNotEquals("Finding subtraction of two number for False Positive", 3, calculator.pow(5,8), DELTA);
-        //assertNotEquals("Finding subtraction of two number for False Positive", -1, calculator.sub(5,4), DELTA);
+
     }
-
-
 
 }
