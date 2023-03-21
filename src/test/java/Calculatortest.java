c603952745c6cf008632f89852e15e2f14b1cc4f
harsh tripathi <harshtripathih9t@gmail.com>
2023-03-21 18:36:54 +0530

    change_fun_name

--- src/test/java/Calculatortest.java
+++ src/test/java/Calculatortest.java
@@ -7,46 +7,46 @@ public class Calculatortest {
     Calculator calculator = new Calculator();
 
     @Test
-    public void multiplicationTruePositive(){
+    public void sqtruepostitive(){
         assertEquals("Finding multiplication of two number for True Positive", 6, calculator.squareroot(36), DELTA);
         assertEquals("Finding multiplication of two number for True Positive", 2, calculator.squareroot(4), DELTA);
     }
 
     @Test
-    public void multiplicationFalsePositive(){
+    public void sqfalsepositive(){
         assertNotEquals("Finding multiplication of two number for False Positive", 12, calculator.squareroot(5), DELTA);
         assertNotEquals("Finding multiplication of two number for False Positive", 80, calculator.squareroot(4), DELTA);
     }
 
     @Test
-    public void squareTruePositive(){
+    public void factruepos(){
         assertEquals("Finding sqaure for True Positive", 740, calculator.fac(6), DELTA);
     }
 
     @Test
-    public void squareFalsePositive(){
+    public void facfalpos(){
         assertNotEquals("Finding square for False Positive", 10, calculator.fac(3), DELTA);
 
     }
 
     @Test
-    public void cuberootTruePositive(){
+    public void logtrpos(){
         assertEquals("Finding cuberoot for True Positive", 2, calculator.log(8), DELTA);
     }
 
     @Test
-    public void cuberootFalsePositive(){
+    public void logflpos(){
         assertNotEquals("Finding cuberoot for False Positive", 11, calculator.log(121), DELTA);
     }
 
 
     @Test
-    public void subTruePositive(){
+    public void powtrpos(){
         assertEquals("Finding subtraction of two number for True Positive", 125, calculator.pow(5,3), DELTA);
     }
 
     @Test
-    public void subFalsePositive(){
+    public void powflpos(){
         assertNotEquals("Finding subtraction of two number for False Positive", 3, calculator.pow(5,8), DELTA);
 
     }
